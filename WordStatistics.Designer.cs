feec06cb0859931c5948681677401a0a5bace692
Jesse Dalton <jmdalton0@gmail.com>
2022-11-19 15:15:44 -0800

    initial commit

---
+++ WordStatistics.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Act5
+{
+    partial class WordStatistics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileLabel = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.openFileTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileLabel
+            // 
+            this.openFileLabel.AutoSize = true;
+            this.openFileLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.openFileLabel.Location = new System.Drawing.Point(76, 66);
+            this.openFileLabel.Name = "openFileLabel";
+            this.openFileLabel.Size = new System.Drawing.Size(165, 20);
+            this.openFileLabel.TabIndex = 0;
+            this.openFileLabel.Text = "Please choose a file:";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.BackColor = System.Drawing.Color.Teal;
+            this.openFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openFileButton.ForeColor = System.Drawing.Color.White;
+            this.openFileButton.Location = new System.Drawing.Point(557, 124);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(142, 57);
+            this.openFileButton.TabIndex = 1;
+            this.openFileButton.Text = "Open";
+            this.openFileButton.UseVisualStyleBackColor = false;
+            this.openFileButton.Click += new System.EventHandler(this.chooseFileButton_Click);
+            // 
+            // openFileTextBox
+            // 
+            this.openFileTextBox.BackColor = System.Drawing.Color.White;
+            this.openFileTextBox.Enabled = false;
+            this.openFileTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.openFileTextBox.Location = new System.Drawing.Point(299, 66);
+            this.openFileTextBox.Name = "openFileTextBox";
+            this.openFileTextBox.Size = new System.Drawing.Size(400, 26);
+            this.openFileTextBox.TabIndex = 2;
+            // 
+            // WordStatistics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(232)))), ((int)(((byte)(231)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.openFileTextBox);
+            this.Controls.Add(this.openFileButton);
+            this.Controls.Add(this.openFileLabel);
+            this.Name = "WordStatistics";
+            this.Text = "Word Statistics";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label openFileLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.TextBox openFileTextBox;
+    }
+}
---
+++ WordStatistics.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Act5
+{
+    public partial class WordStatistics : Form
+    {
+
+        private String filename;
+
+        public WordStatistics()
+        {
+            InitializeComponent();
+        }
+
+        private void chooseFileButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog.InitialDirectory = "C:\\Users\\jmdal\\Projects\\CST150\\Topic3\\Act5\\rsrc";
+            openFileDialog.FileName = "";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filename = openFileDialog.FileName;
+                openFileTextBox.Text = filename;
+            }
+            else
+            {
+                openFileTextBox.Text = "no file selected";
+            }
+        }
+    }
+}
