a13aa888a4bd42f9db13667eabca146f56562ef0
Jesse Dalton <jmdalton0@gmail.com>
2022-11-19 16:03:06 -0800

    feature: read words from text file

--- WordStatistics.Designer.cs
+++ WordStatistics.Designer.cs
@@ -32,6 +32,12 @@
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.openFileButton = new System.Windows.Forms.Button();
             this.openFileTextBox = new System.Windows.Forms.TextBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.processButton = new System.Windows.Forms.Button();
+            this.firstAlphaLabel = new System.Windows.Forms.Label();
+            this.lastAlphaWord = new System.Windows.Forms.Label();
+            this.firstAlphaTextBox = new System.Windows.Forms.TextBox();
+            this.lastAlphaTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // openFileLabel
@@ -53,7 +59,7 @@
             this.openFileButton.BackColor = System.Drawing.Color.Teal;
             this.openFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.openFileButton.ForeColor = System.Drawing.Color.White;
-            this.openFileButton.Location = new System.Drawing.Point(557, 124);
+            this.openFileButton.Location = new System.Drawing.Point(80, 116);
             this.openFileButton.Name = "openFileButton";
             this.openFileButton.Size = new System.Drawing.Size(142, 57);
             this.openFileButton.TabIndex = 1;
@@ -64,19 +70,93 @@
             // openFileTextBox
             // 
             this.openFileTextBox.BackColor = System.Drawing.Color.White;
+            this.openFileTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.openFileTextBox.Enabled = false;
             this.openFileTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.openFileTextBox.Location = new System.Drawing.Point(299, 66);
             this.openFileTextBox.Name = "openFileTextBox";
-            this.openFileTextBox.Size = new System.Drawing.Size(400, 26);
+            this.openFileTextBox.Size = new System.Drawing.Size(665, 26);
             this.openFileTextBox.TabIndex = 2;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.statusLabel.Location = new System.Drawing.Point(308, 278);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(105, 20);
+            this.statusLabel.TabIndex = 3;
+            this.statusLabel.Text = "Processing...";
+            this.statusLabel.Visible = false;
+            // 
+            // processButton
+            // 
+            this.processButton.BackColor = System.Drawing.Color.Teal;
+            this.processButton.Enabled = false;
+            this.processButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.processButton.ForeColor = System.Drawing.Color.White;
+            this.processButton.Location = new System.Drawing.Point(80, 258);
+            this.processButton.Name = "processButton";
+            this.processButton.Size = new System.Drawing.Size(142, 57);
+            this.processButton.TabIndex = 4;
+            this.processButton.Text = "Process";
+            this.processButton.UseVisualStyleBackColor = false;
+            this.processButton.Click += new System.EventHandler(this.processButton_Click);
+            // 
+            // firstAlphaLabel
+            // 
+            this.firstAlphaLabel.AutoSize = true;
+            this.firstAlphaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.firstAlphaLabel.Location = new System.Drawing.Point(76, 358);
+            this.firstAlphaLabel.Name = "firstAlphaLabel";
+            this.firstAlphaLabel.Size = new System.Drawing.Size(184, 20);
+            this.firstAlphaLabel.TabIndex = 5;
+            this.firstAlphaLabel.Text = "First Alphabetical Word";
+            // 
+            // lastAlphaWord
+            // 
+            this.lastAlphaWord.AutoSize = true;
+            this.lastAlphaWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lastAlphaWord.Location = new System.Drawing.Point(76, 410);
+            this.lastAlphaWord.Name = "lastAlphaWord";
+            this.lastAlphaWord.Size = new System.Drawing.Size(183, 20);
+            this.lastAlphaWord.TabIndex = 6;
+            this.lastAlphaWord.Text = "Last Alphabetical Word";
+            // 
+            // firstAlphaTextBox
+            // 
+            this.firstAlphaTextBox.BackColor = System.Drawing.Color.White;
+            this.firstAlphaTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.firstAlphaTextBox.Enabled = false;
+            this.firstAlphaTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.firstAlphaTextBox.Location = new System.Drawing.Point(312, 356);
+            this.firstAlphaTextBox.Name = "firstAlphaTextBox";
+            this.firstAlphaTextBox.Size = new System.Drawing.Size(328, 26);
+            this.firstAlphaTextBox.TabIndex = 7;
+            // 
+            // lastAlphaTextBox
+            // 
+            this.lastAlphaTextBox.BackColor = System.Drawing.Color.White;
+            this.lastAlphaTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lastAlphaTextBox.Enabled = false;
+            this.lastAlphaTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lastAlphaTextBox.Location = new System.Drawing.Point(312, 408);
+            this.lastAlphaTextBox.Name = "lastAlphaTextBox";
+            this.lastAlphaTextBox.Size = new System.Drawing.Size(328, 26);
+            this.lastAlphaTextBox.TabIndex = 8;
             // 
             // WordStatistics
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(232)))), ((int)(((byte)(231)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1085, 693);
+            this.Controls.Add(this.lastAlphaTextBox);
+            this.Controls.Add(this.firstAlphaTextBox);
+            this.Controls.Add(this.lastAlphaWord);
+            this.Controls.Add(this.firstAlphaLabel);
+            this.Controls.Add(this.processButton);
+            this.Controls.Add(this.statusLabel);
             this.Controls.Add(this.openFileTextBox);
             this.Controls.Add(this.openFileButton);
             this.Controls.Add(this.openFileLabel);
@@ -93,5 +173,11 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog;
         private System.Windows.Forms.Button openFileButton;
         private System.Windows.Forms.TextBox openFileTextBox;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button processButton;
+        private System.Windows.Forms.Label firstAlphaLabel;
+        private System.Windows.Forms.Label lastAlphaWord;
+        private System.Windows.Forms.TextBox firstAlphaTextBox;
+        private System.Windows.Forms.TextBox lastAlphaTextBox;
     }
 }
--- WordStatistics.cs
+++ WordStatistics.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Act5
@@ -14,14 +11,21 @@ namespace Act5
     {
 
         private String filename;
+        private List<string> words;
 
         public WordStatistics()
         {
             InitializeComponent();
+            words = new List<string>();
+            processButton.Enabled = false;
+            processButton.BackColor = Color.WhiteSmoke;
+            statusLabel.Visible = false;
         }
 
         private void chooseFileButton_Click(object sender, EventArgs e)
         {
+            statusLabel.Visible = false;
+
             openFileDialog.InitialDirectory = "C:\\Users\\jmdal\\Projects\\CST150\\Topic3\\Act5\\rsrc";
             openFileDialog.FileName = "";
 
@@ -29,10 +33,45 @@ namespace Act5
             {
                 filename = openFileDialog.FileName;
                 openFileTextBox.Text = filename;
+                processButton.Enabled = true;
+                processButton.BackColor = Color.Teal;
             }
             else
             {
                 openFileTextBox.Text = "no file selected";
+                processButton.Enabled = false;
+                processButton.BackColor = Color.WhiteSmoke;
+            }
+        }
+
+        private void processButton_Click(object sender, EventArgs e)
+        {
+            statusLabel.Text = "Processing...";
+            statusLabel.Visible = true;
+
+            try
+            {
+                StreamReader inFile = File.OpenText(filename);
+                while(!inFile.EndOfStream)
+                {
+                    words.Add(inFile.ReadLine());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            statusLabel.Text = "Finished.";
+
+            processWords();
+        }
+
+        private void processWords()
+        {
+            for (int i = 0; i < words.Count; i++)
+            {
+                Console.WriteLine(words.ElementAt(i));
             }
         }
     }
